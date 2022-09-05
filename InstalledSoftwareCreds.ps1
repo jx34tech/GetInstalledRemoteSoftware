b2df9837f2b95d14c433d889d334c4664c99a407
Jack Sullivan <jack.sullivan@uon.edu.au>
2022-09-05 10:41:45 +1000

    Adding ability for log or screen output

--- InstalledSoftwareCreds.ps1
+++ InstalledSoftwareCreds.ps1
@@ -1,6 +1,15 @@
 $Credential = $host.ui.PromptForCredential("Need credentials", "Please enter your user name and password.", "", "NetBiosUserName")
 $compName = Read-Host "Enter the Computer Name: "
-Write-Output "Getting installed software ..."
-$output = Get-WmiObject Win32_Product -ComputerName $compName -Credential $Credential | select Name,Version
-Write-Output $output | Out-Host
-Pause
+$saveLocation = Read-Host "Please type where to save; screen or log"
+if ($saveLocation == "screen"){
+    Write-Output "Getting installed software ..."
+    $output = Get-WmiObject Win32_Product -ComputerName $compName -Credential $Credential | select Name,Version
+    Write-Output $output | Out-Host
+    Pause
+}else{
+    Write-Output "Getting installed software ..."
+    $logFileLocation = $compName + ".txt"
+    $output = Get-WmiObject Win32_Product -ComputerName $compName -Credential $Credential | select Name,Version | Out-File -Path $logFileLocation
+    Write-Output $output | Out-Host
+    Pause
+}
