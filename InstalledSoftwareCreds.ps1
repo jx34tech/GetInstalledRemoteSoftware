7b98a6619f673c5ab14e427f2ad5cb30171b86a6
Jack Sullivan <jack.sullivan@uon.edu.au>
2022-09-05 10:46:40 +1000

    Correct path variable and comparison operator

--- InstalledSoftwareCreds.ps1
+++ InstalledSoftwareCreds.ps1
@@ -1,7 +1,7 @@
 $Credential = $host.ui.PromptForCredential("Need credentials", "Please enter your user name and password.", "", "NetBiosUserName")
 $compName = Read-Host "Enter the Computer Name: "
 $saveLocation = Read-Host "Please type where to save; screen or log"
-if ($saveLocation == "screen"){
+if ($saveLocation -eq "screen"){
     Write-Output "Getting installed software ..."
     $output = Get-WmiObject Win32_Product -ComputerName $compName -Credential $Credential | select Name,Version
     Write-Output $output | Out-Host
@@ -9,7 +9,7 @@ if ($saveLocation == "screen"){
 }else{
     Write-Output "Getting installed software ..."
     $logFileLocation = $compName + ".txt"
-    $output = Get-WmiObject Win32_Product -ComputerName $compName -Credential $Credential | select Name,Version | Out-File -Path $logFileLocation
+    $output = Get-WmiObject Win32_Product -ComputerName $compName -Credential $Credential | select Name,Version | Out-File $logFileLocation
     Write-Output $output | Out-Host
     Pause
 }
